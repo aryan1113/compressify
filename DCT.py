4403211fdd30f618217738b92c8a2c1d7d1540d4
Aryan <aryannewcollege@gmail.com>
2024-10-19 03:29:31 +0530

    Feat: add FFT
    FFT makes use of divide and conquer approach to break down the problem into small chunks, making use of the property the FFT(a, b) = (a+b), (a-b). Further testing needs to be done, preferably in a separate file

--- DCT.py
+++ DCT.py
@@ -43,19 +43,75 @@ def dct2D(input_matrix):
             C_u = 1 / math.sqrt(2) if u == 0 else 1
             C_v = 1 / math.sqrt(2) if v == 0 else 1
             
-            output_matrix[u][v] = round(1 / 4.0 * Cu * Cv * sum_value, 2)
+            output_matrix[u][v] = round(1 / 4.0 * C_u * C_v * sum_value, 2)
     return output_matrix
 
 
+def fft1D(sequence):
+    N = len(sequence)
+    if N <= 1:
+        return sequence
+
+    # FFT for even indexed elements
+    even = fft1D(sequence[0::2])
+
+    # FFT for odd indexed elements
+    odd = fft1D(sequence[1::2])  
+
+    T = [np.exp(-2j * np.pi * k / N) * odd[k] for k in range(N // 2)]
+    return [even[k] + T[k] for k in range(N // 2)] + [even[k] - T[k] for k in range(N // 2)]
+
+def fft2D(input_matrix):
+    rows_fft = [ fft1D(row) for row in input_matrix ]
+    cols_fft = [fft1D(col) for col in np.array(rows_fft).T]
+    
+    magnitude = np.array(cols_fft).T
+    return magnitude
+
+# trash code, to test stuff
+# later can be moved to a separate testing file
+
+# input_matrix = [
+# [144,	139,	149,	155,	153,	155,    155,	155],
+# [151,	151,	151,	159,	156,	156,	156,	158],
+# [151,	156,    160,	162,	159,	151,	151,	151],
+# [158,	163,    161,	160,	160,	160,	160,	161],
+# [158,	160,	161,	162,	160,	155,	155,	156],
+# [161,	161,	161,    161,    160,	157,	157,	157],
+# [162,	162,	161,	160,	161,	157,	157,	157],
+# [162	,162,	161,	160,	163,	157,	158,	154]]
+
+# np.set_printoptions(suppress=True)
+
+# # built in library under numyp
+# print("NumPy FFT Result:")
+# np_fft_result = np.fft.fft2(input_matrix)
+# np_magnitude = np.abs(np_fft_result)
+# np_magnitude = np.round(np_magnitude, 3)
+
+# print(np_magnitude)
+
+# print()
+
+# print("Our FFT Implementation Results are: ")
+# our_implementation = np.abs(fft2D(input_matrix))
+# our_implementation= np.round(our_implementation, 3)
+# print(our_implementation)
+
+# print(np_fft_result == our_implementation)
 
 
-input_matrix = [
-[144,	139,	149,	155,	153,	155,    155,	155],
-[151,	151,	151,	159,	156,	156,	156,	158],
-[151,	156,    160,	162,	159,	151,	151,	151],
-[158,	163,    161,	160,	160,	160,	160,	161],
-[158,	160,	161,	162,	160,	155,	155,	156],
-[161,	161,	161,    161,    160,	157,	157,	157],
-[162,	162,	161,	160,	161,	157,	157,	157],
-[162	,162,	161,	160,	163,	157,	158,	154]]
-print(dct2D(input_matrix))
+
+# '''
+# Compare DCT and fFT results
+
+# FFT results seem a bit too large, this might negatively affect the quantization process
+# but this is something we can afford over a large computation improvement
+# '''
+# print("FFT results from our implemenation are :")
+# print(np_magnitude)
+
+# dct_implementation = np.round( dct2D(input_matrix), 3)
+# print("DCT results from our implementation are: ")
+# print(dct_implementation)
+# print(np_magnitude == dct_implementation)
